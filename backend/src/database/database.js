d955c6dfa21c44a0985d4ea57eec76d91c10d6f1
Francisco <Francisco@DESKTOP-EA6GVHP>
2020-03-25 15:47:45 -0300

    Cambiado puerto de BD

--- backend/src/database/database.js
+++ backend/src/database/database.js
@@ -7,7 +7,7 @@ const sequelize = new Sequelize(
     'investigacion',//password
     {
         host:'localhost',
-        port:5432,
+        port:5433,
         dialect:'postgres',
         pool:{
             max:5,
