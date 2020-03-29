a2bbbb38047083d6c74d1a28b81e120550bda313
Juan Manuel <juanmix.01@gmail.com>
2020-03-29 16:47:00 -0300

    form ensayo added

--- client/src/app/pages/ensayo/ensayo.page.ts
+++ client/src/app/pages/ensayo/ensayo.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Ensayo } from '../../interfaces/interfaces';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-ensayo',
@@ -7,9 +8,28 @@ import { Ensayo } from '../../interfaces/interfaces';
   styleUrls: ['./ensayo.page.scss'],
 })
 export class EnsayoPage implements OnInit {
-  nroEnsayo:number;
+  formularioEnsayo: FormGroup;
 
-  constructor() { }
+  constructor() { 
+    this.formularioEnsayo = new FormGroup({
+      'nroEnsayo': new FormControl('',Validators.required),
+      'fecha': new FormControl(),
+      'operador': new FormControl('',Validators.required),
+      'observaciones': new FormControl(),
+      'distanciaTotal': new FormControl('',Validators.required),
+      'radioTrayectoria': new FormControl('',Validators.required),
+      'materialBola': new FormControl('',Validators.required),
+      'carga': new FormControl('', Validators.required),
+      'diametroBola': new FormControl('',Validators.required),
+      'Probeta': new FormGroup({
+        'codigoProbeta': new FormControl('',Validators.required),
+        'dureza': new FormControl('',Validators.required),
+        'tratamiento': new FormControl('', Validators.required),
+        'materialProbeta': new FormControl('',Validators.required)
+      })
+
+    });
+  }
 
   ngOnInit() {
   }
