65e6125a2b09db7c479ad405cf6204ee5d904b81
Juan Manuel <juanmix.01@gmail.com>
2020-10-09 17:03:02 -0300

    fix: fixed test

--- server/src/testserial2.ts
+++ server/src/testserial2.ts
@@ -4,7 +4,7 @@ import Ensayo from "./models/Ensayo";
 import Ambiente from "./models/Ambiente";
 import moment from "moment";
 import { Observable, Subscription } from "rxjs";
-const elPuerto: SerialPort = new SerialPort(port.puertoCelda, {
+const elPuerto: SerialPort = new SerialPort(port.puertoControlador, {
    //autoOpen:false
 })
 
