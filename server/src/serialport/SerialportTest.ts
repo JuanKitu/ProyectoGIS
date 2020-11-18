faa04060e8d07cd65e9ec96656b235e2b1cbd31e
Juan Manuel <juanmix.01@gmail.com>
2020-11-18 13:39:37 -0300

    fix: arreglo de error en script de test

--- server/src/serialport/SerialportTest.ts
+++ server/src/serialport/SerialportTest.ts
@@ -2,8 +2,10 @@ import SerialPort from "serialport";
 import { port } from '../interfaces/interfaces';
 
 const portControlador = new SerialPort(port.puertoControlador, {
+    autoOpen:false,
     baudRate: 9600
 });
+portControlador.open();
 console.log('Testeando');
 portControlador.write('<TEST>\n');
 portControlador.on('data',(data)=>{
