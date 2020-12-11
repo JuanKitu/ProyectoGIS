b0ac1c06dc5dcb37fe25839f7cd3052b9deec27c
Francisco <Francisco@DESKTOP-EA6GVHP>
2020-12-11 11:57:34 -0300

    feat: Agregados scripts de prueba

---
+++ server/scriptDesconectar.ts
@@ -0,0 +1,13 @@
+import SerialPort from 'serialport';
+import { port } from './src/interfaces/interfaces';
+let portControlador = new SerialPort(port.puertoControlador, {
+    baudRate: 9600,
+});
+
+portControlador.write('<DCON>\n');
+portControlador.on('readable', () => {
+    const control = portControlador.read();
+    if (control) {
+        console.log(control.toString());
+    };
+});
