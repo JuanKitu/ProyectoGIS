8e523c43d338e7993c2a7d372b2b6b31474da204
Juan Manuel <juanmix.01@gmail.com>
2020-12-14 15:06:06 -0300

    fix: cambiado a modo flowing el script de fuerza

--- server/src/scriptFuerza.ts
+++ server/src/scriptFuerza.ts
@@ -4,8 +4,7 @@ let portCelda = new SerialPort(port.puertoCelda, {
     baudRate: 9600,
 });
 setInterval(() => {
-    portCelda.on('readable', () => {
-        let data=portCelda.read();
+    portCelda.on('data', (data) => {
         if(data){
             console.log(data.toString());
         }else{
