5c32e138652abb5fac5c3ec9f05e9edf86c0760c
Juan Manuel <juanmix.01@gmail.com>
2020-12-14 12:48:25 -0300

    fix: cambiado unlink a synch

--- server/src/serialport/Serialport.ts
+++ server/src/serialport/Serialport.ts
@@ -44,8 +44,8 @@ async function comenzarExperimeto(puerto: SerialPort, ensayo: Ensayo) {
         console.log('Vueltas', vueltas, puerto.isOpen);
             iniciar(unRadio, vueltas, puerto).then(data2 => {
                 estadoScript = 1;
-                fs.unlink('vueltas.json');
-                fs.unlink('fuerzas.json');
+                fs.unlinkSync('vueltas.json');
+                fs.unlinkSync('fuerzas.json');
                 console.log('Vueltas2');
                 const childFuerza = fork('dist/serialport/SerialportFuerza.js');
                 const childVuelta = fork('dist/serialport/SerialportVueltas.js');
